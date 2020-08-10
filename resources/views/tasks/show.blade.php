f5515c856d5aebc946cb8801a18701060e3b6a4c
tomoyuki/ogawa <prime1992311to@gmail.com>
2020-08-10 07:36:27 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -11,7 +11,7 @@
         </tr>
         <tr>
             <th>ステータス</th>
-            <td>{{ $status->title }}</td>
+            <td>{{ $task->status }}</td>
         </tr>
         <tr>
             <th>タスク</th>
